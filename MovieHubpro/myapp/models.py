89652b3e3e8b9657223508207ba228d26b8d749a
Vyshnav Rajesh <vyshnavinquiries@gmail.com>
2023-04-10 19:35:05 +0530

    added a new model Reviews

--- MovieHubpro/myapp/models.py
+++ MovieHubpro/myapp/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
@@ -28,4 +29,10 @@ class Movies(models.Model):
     description=models.CharField(max_length=200,null=True)
 
     def __str__(self):
-        return self.name   
+        return self.name  
+
+class Reviews(models.Model):
+    movie=models.ForeignKey(Movies,on_delete=models.CASCADE)     
+    user=models.ForeignKey(User,on_delete=models.CASCADE)  
+    comment=models.CharField(max_length=250)
+    rating=models.PositiveIntegerField()   
